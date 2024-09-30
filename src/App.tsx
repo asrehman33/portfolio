bfd9df8b25cc917fc2e3121cc54265b9af257221
REHMAN Asad-ur <asad-ur.rehman@etu.u-bordeaux.fr>
2024-09-30 22:07:48 +0200

    modif navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import MyNavbar from './components/Navbar';
 import Home from './components/Home';
 import UniversityProjects from './components/UniversityProjects';
 import PersonalProjects from './components/PersonalProjects';
 import Contact from './components/Contact';
-import Background from './components/BackgroundDarkd';
 
 function App() {
   return (
     <Router>
       <MyNavbar />
       <Routes>
-        <Route path="/PortfolioReact" element={<Home />} />
-        <Route path="/PortfolioReact/university-projects" element={<UniversityProjects />} />
-        <Route path="/PortfolioReact/personal-projects" element={<PersonalProjects />} />
-        <Route path="/PortfolioReact/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/university-projects" element={<UniversityProjects />} />
+        <Route path="/personal-projects" element={<PersonalProjects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
