4a494a87df421691ff905d0b63f7e205d9439ae3
REHMAN Asad-ur <asad-ur.rehman@etu.u-bordeaux.fr>
2024-10-03 20:38:47 +0200

    add navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 import './css/Navbar.css';
 
 const MyNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const getLink = (path: string) => {
+    return `${path}`;
+  };
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className='navbar'>
-      <Navbar.Brand href="/" className="brand-text">Asad-ur Rehman</Navbar.Brand>
+      <Navbar.Brand as={Link} to={getLink("/")} className="brand-text">Asad-ur Rehman</Navbar.Brand>
       <Navbar.Toggle 
         aria-controls="basic-navbar-nav" 
         className={`toggle-button ${isOpen ? 'active' : ''}`} 
@@ -19,10 +25,10 @@ const MyNavbar = () => {
       />
       <Navbar.Collapse id="basic-navbar-nav" className={`justify-content-center ${isOpen ? 'show' : ''}`}>
         <Nav className={`navbar-links ${isOpen ? 'show' : ''}`}>
-          <Nav.Link href="/">Accueil</Nav.Link>
-          <Nav.Link href="/university-projects">Projets universitaires</Nav.Link>
-          {/* <Nav.Link href="/personal-projects">Projets personnels</Nav.Link> */}
-          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to={getLink("/")}>Accueil</Nav.Link>
+          <Nav.Link as={Link} to={getLink("/university-projects")}>Projets universitaires</Nav.Link>
+          {/* <Nav.Link as={Link} to={getLink("/personal-projects")}>Projets personnels</Nav.Link> */}
+          <Nav.Link as={Link} to={getLink("/contact")}>Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
